378148e45f3b7a24041360faf591478fd48df540
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-18 14:56:57 +0100

    fine primo esercizio

---
+++ Palidroma/main.js
@@ -0,0 +1,22 @@
+function palindroma(parola) {
+    let split=parola.split("")
+    let reverse=split.reverse()
+    parolaAlContrario= reverse.join("")
+    console.log(parolaAlContrario)
+    if(parola==parolaAlContrario){
+        return "ok"
+    } else{
+        return "no"
+    }
+
+}
+let parola=prompt("inserisci una parola")
+console.log(palindroma(parola))
+
+if(palindroma(parola) == "ok"){
+    document.getElementById("cont").innerHTML="la parola è palindroma"
+} else{
+    document.getElementById("cont").innerHTML="la parola non è palindroma"
+}
+
+
